26c172cb2f2d382484e68ca4006c10efd2b6e4a8
Yug <03yugagarwal@gmail.com>
2022-06-29 10:42:36 +0530

    added basic functionality with sample json

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react'
 import "./css/NewsItem.css"
 
 export class NewsItem extends Component {
+
+
     render() {
-        let {title,description,imageUrl} = this.props;
+        let {title,description,imageUrl,newsUrl} = this.props;
         return (
             <div className="news-item-card">
-                <img src={imageUrl} alt="" className="news-item-img" />
-                <h4 className="news-item-heading">{title}</h4>
-                <p className="news-item-p">{description}</p>
-                <a href="/newsItem">IDK</a>
+                <div className="news-item-image-div">
+                    <img src={imageUrl} alt="img" className="news-item-img" />
+                </div>
+                <div className="news-item-other-components">
+                    <hr className="news-item-hr"/>
+                    <h4 className="news-item-heading">{title}</h4>
+                    <p className="news-item-p">{description}</p>
+                    <hr className="news-item-hr"/>
+                    <a href={newsUrl} target="_blanck" >Read More</a>
+                </div>
             </div>
     )
   }
